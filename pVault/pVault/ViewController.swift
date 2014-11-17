86034a7aba08d310395e1ef3d3ce966b6905afb4
Joseph ORLANDO <josephorlando@Josephs-MacBook-Pro.local>
2014-11-16 16:00:17 -0800

    Created Swift Project

---
+++ pVault/pVault/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pVault
+//
+//  Created by Joseph ORLANDO on 11/16/14.
+//  Copyright (c) 2014 Pvault2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
