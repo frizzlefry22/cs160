9047fba5089ab651c237a100c442ab80349b0ac9
arjaynguyen123@hotmail.com <arjaynguyen123@hotmail.com>
2014-11-19 19:39:23 -0800

    -added frameworks for Parse -added DatabaseConnection superclass 	.init key constants 	.init CRED methods 	.not completed

---
+++ pVault/pVault/DatabaseConnection.swift
@@ -0,0 +1,31 @@
+//
+//  DatabaseConnection.swift
+//  pVault
+//
+//  Created by Arjay Nguyen on 11/19/14.
+//  Copyright (c) 2014 Pvault2. All rights reserved.
+//
+
+import Foundation
+
+class DatabaseConnection{
+    let appKey = "oYyJc42HMA5WVZrufqQv5UtJt8CRNIUpkJyklqTd" //application key to connect to parseDB
+    let clientKey = "cSMbV7VGBEEboCA3KYlbIWzu4ljHd0BVwBYdPUhf" //client key to connect to parseDB
+    
+    func create(){
+        
+    }
+    
+    func read(){
+        
+    }
+    
+    func edit(){
+        
+    }
+    
+    func delete(){
+        
+    }
+    
+}
