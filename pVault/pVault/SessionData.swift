4d9d864d9fd02ee969397bd69519e14cdbdb7927
arjaynguyen123@hotmail.com <arjaynguyen123@hotmail.com>
2014-11-29 21:49:06 -0800

    created User model and SessionData

---
+++ pVault/pVault/SessionData.swift
@@ -0,0 +1,14 @@
+//
+//  SessionData.swift
+//  pVault
+//
+//  Holds all data used during current session(i.e. User, Documents, etc)
+//
+//  Created by Arjay Nguyen on 11/29/14.
+//  Copyright (c) 2014 Pvault2. All rights reserved.
+//
+
+import Foundation
+
+
+var user = User()
